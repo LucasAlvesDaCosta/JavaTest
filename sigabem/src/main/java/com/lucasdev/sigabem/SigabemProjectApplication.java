8548117f201ebb90ba2d82292542c1f8edb3994a
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2022-02-03 18:05:56 -0300

    Project created

---
+++ sigabem/src/main/java/com/lucasdev/sigabem/SigabemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdev.sigabem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigabemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigabemProjectApplication.class, args);
+	}
+
+}
---
+++ sigabem/src/test/java/com/lucasdev/sigabem/SigabemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdev.sigabem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigabemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
